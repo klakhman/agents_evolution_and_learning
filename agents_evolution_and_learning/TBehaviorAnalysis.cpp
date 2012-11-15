0b024387701b6c125bb38698cd2c5b004558209d
Nikita Pestrov <ipestrov@gmail.com>
2012-11-15 14:54:54 +0400

    First level of Cycles Detection abstraction finished
    - TBehaviorAnalysis::findCyclesInPopulation created with the concept of
    "wishful thinking"
    - To be continued on lower level

--- agents_evolution_and_learning/TBehaviorAnalysis.cpp
+++ agents_evolution_and_learning/TBehaviorAnalysis.cpp
@@ -20,6 +20,7 @@ void TBehaviorAnalysis::beginAnalysis(int argc, char **argv)
 {
   //Извлекаем данные из файлов и определяем режим анализа
   decodeCommandPromt(argc, argv);
+  
   //В зависимости от выбранного режима запускаем процедуры анализа
    switch (mode) {
      case TBAModePoulation: {
@@ -28,8 +29,12 @@ void TBehaviorAnalysis::beginAnalysis(int argc, char **argv)
        //!!! Обнуляем степень стохастичности среды (чтобы все было детерминировано)
        environment->setStochasticityCoefficient(0.0);
        TPopulation* agentsPopulation = new TPopulation;
-
-      findCyclesInPopulation(*agentsPopulation);
+       settings::fillPopulationSettingsFromFile(*agentsPopulation, filenameSettings.settingsFilename);
+       // Физически агенты в популяции уже созданы (после того, как загрузился размер популяции), поэтому можем загрузить в них настройки
+       settings::fillAgentsPopulationSettingsFromFile(*agentsPopulation, filenameSettings.settingsFilename);
+       agentsPopulation->loadPopulation(filenameSettings.populationFilename);
+       //Запускаем процесс поиска
+       findCyclesInPopulation(*agentsPopulation, *environment);
       break;
      }
     case TBAModeSingleAgent:
@@ -38,12 +43,40 @@ void TBehaviorAnalysis::beginAnalysis(int argc, char **argv)
       break;
   }
 }
-SCycle TBehaviorAnalysis::findCycleInAgentLife(double *agentLife,int lifeTime)
+vector<SCycle> TBehaviorAnalysis::findCyclesInPopulation(TPopulation &population, TEnvironment &environment)
+{  
+  //Вычисляем размер популяции единожды
+  int populationSize = population.getPopulationSize();
+  //Создаем вектор циклов
+  detectedCycles = *new vector<SCycle>;
+  //Прогрняем всех агентов в популяции
+  for (int agentNumber = 0; agentNumber < populationSize; ++agentNumber) {
+    //Запускаем прогон агента с целью поиска циклов
+    vector<SCycle> currentAgentCycles = findAllCyclesOfAgent(*population.getPointertoAgent(agentNumber), environment);
+    //В случае нахождения циклов, проверяем их на уникальность и добавляем в основной вектор циклов
+    if (currentAgentCycles.size()) {
+      for (vector<SCycle>::iterator singleCycle = currentAgentCycles.begin(); singleCycle!=currentAgentCycles.end();++singleCycle) {
+        if (checkCycleUniqueness(*singleCycle))
+          detectedCycles.push_back(*singleCycle);
+      }
+    }
+  }
+  
+  return detectedCycles;
+}
+vector<SCycle> TBehaviorAnalysis::findAllCyclesOfAgent(TAgent &agent, TEnvironment &environment)
+{
+  //Создаем первичный репертуар
+  agent.linearSystemogenesis();
+}
+SCycle TBehaviorAnalysis::findCycleInAgentLife(TAgent &agent, TEnvironment &environment){
+}
+SCycle TBehaviorAnalysis::findCycleInSequence(double *sequence, int sequenceLength)
 {
   vector<double> cycle(maxCycleLength);
   SCycle theCycle;
   //Указатель на конец жизни агента, он, вообще говоря, выходит за рамки массива, не знаю, насколько это безопасно, но зато не надо с единичками возиться
-  double *endOfLifePointer = agentLife+lifeTime;
+  double *endOfLifePointer = sequence+sequenceLength;
   
   //Начинаем с максимальной длины и идем до цикла длиной в два действия
   for (int cycleLength = maxCycleLength; cycleLength > 2; --cycleLength) {
@@ -64,42 +97,18 @@ SCycle TBehaviorAnalysis::findCycleInAgentLife(double *agentLife,int lifeTime)
     }
     //Проверяем, встретилась ли последовательность достаточное количество раз
     if (repeatCount == sufficientRepeatNumber) {
-     // theCycle.cycleSequence = cycle;
+      // theCycle.cycleSequence = cycle;
       return theCycle;
     }
   }
   //Вот тут нужно возвращать пустой какой-то объект - подумаю над этим завтра - просто не знаю, как это лучше оформить)
   return theCycle;
-}
-vector<SCycle> TBehaviorAnalysis::findCyclesInPopulation(TPopulation &population){
-  
-	TEnvironment* environment = new TEnvironment(filenameSettings.environmentFilename);
-	settings::fillEnvironmentSettingsFromFile(*environment, filenameSettings.settingsFilename);
-	//!!! Обнуляем степень стохастичности среды (чтобы все было детерминировано)
-	environment->setStochasticityCoefficient(0.0);
-	TPopulation* agentsPopulation = new TPopulation;
-	settings::fillPopulationSettingsFromFile(*agentsPopulation, filenameSettings.settingsFilename);
-	// Физически агенты в популяции уже созданы (после того, как загрузился размер популяции), поэтому можем загрузить в них настройки
-	settings::fillAgentsPopulationSettingsFromFile(*agentsPopulation, filenameSettings.settingsFilename);
-  
-	agentsPopulation->loadPopulation(filenameSettings.populationFilename);
-  
-  agentsPopulation->getPointertoAgent(3)->linearSystemogenesis();
-  //Решил третьего проверить, например
-  TAgent *agent = agentsPopulation->getPointertoAgent(3);
-  agent->life(*environment, 250);
-  cout<<"YES";
-  cout<<endl;
-  SCycle cycle = findCycleInAgentLife(agent->getPointerToAgentLife(),250);
-  vector<SCycle> cycles = *new vector<SCycle>;
-  cycles.push_back(cycle);
-//  for (int initialState = 0;initialState<cycle.size();initialState++)
-//    cout<<cycle[initialState]<<" ";
-  
-  //Пока ничего путного не возвращаем, создан только базовый метод
-  return cycles;
-}
 
+}
+bool TBehaviorAnalysis::checkCycleUniqueness(SCycle &cycleToAdd)
+{
+  return true;
+}
 void TBehaviorAnalysis::decodeCommandPromt(int argc, char** argv){
   int currentArgNumber = 1; // Текущий номер параметра
   while (currentArgNumber < argc){
--- agents_evolution_and_learning/TBehaviorAnalysis.h
+++ agents_evolution_and_learning/TBehaviorAnalysis.h
@@ -18,13 +18,16 @@ static const int maxCycleLength = 70;
 static const int sufficientRepeatNumber = 4;
 //Максимально возможное число циклов впринципе
 static const int maxCyclesQuantity = 3500;
+//Длина жизни агента для прогона
+static const int agentLifeLength = 350;
 
 //Раздичные режимы анализа
 typedef enum {
   TBAModePoulation,//Поиск циклов во всей популяции
   TBAModeSingleAgent,//Поиск циклов в одном агенте
 }TBehaviorAnalysisMode;
-// Настройки файлов процесса
+
+//Структура цикла
 struct SCycle{
   //Последовательность действий в цикле
   double cycleSequence[maxCycleLength];
@@ -33,17 +36,15 @@ struct SCycle{
   //Награда, получаемая за прохождение цикла
   double cycleReward;
 };
-/*
- Класс анализа поведенческих циклов агента
-*/
+
+/* Класс анализа поведенческих циклов агента */
 class TBehaviorAnalysis{
   
   //Режим анализа - всей популции, одного агента, и т.д.
   TBehaviorAnalysisMode mode;
-  //Структура цикла
-    
-//Определяет режим запуска и файлы с параметрами и данными
-void decodeCommandPromt(int argc, char** argv);
+  //Определяет режим запуска и файлы с параметрами и данными
+  void decodeCommandPromt(int argc, char** argv);
+  // Настройки файлов процесса
 public:
 	struct SFilenameSettings{
 		std::string environmentFilename;
@@ -52,14 +53,21 @@ public:
     std::string agentsFilename;
 		std::string settingsFilename;
 	} filenameSettings;
-
+  
+  std::vector<SCycle> detectedCycles;
+  
   //Запустить процесс анализа, какой именно - указано в argv
   void beginAnalysis(int argc, char **argv);
   //Найти все циклы в популяции путем прогона агентов через из всех начальных состояний среды
-  std::vector<SCycle> findCyclesInPopulation(TPopulation &population);
+  std::vector<SCycle> findCyclesInPopulation(TPopulation &population, TEnvironment &environment);
+  //Прогон агента из всех возможных состояний среды для обнаружения всех возможных циклов
+  std::vector<SCycle> findAllCyclesOfAgent(TAgent &agent, TEnvironment &environment);
   //Находит цикл в жизни данного агента
-  SCycle findCycleInAgentLife(double *agentLife,int lifeTime);
-
+  SCycle findCycleInAgentLife(TAgent &agent, TEnvironment &environment);
+  //Вспомогательная процедура для поиска цикла в последовательности
+  SCycle findCycleInSequence(double *sequence,int sequenceLength);
+  //Проверить, нет ли уже этого цикла в массиве циклов
+  bool checkCycleUniqueness(SCycle &cycleToAdd);
   TBehaviorAnalysis() {};
 	~TBehaviorAnalysis() {};
 };
